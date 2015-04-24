d73e3d164871a9d9abe73e5186c2316686d03435
King Chung Huang <kchuang@ucalgary.ca>
2015-04-24 14:47:06 -0600

    Implement fuzzy matching in listitem_ref
    If fuzzy is True and an exact match is not found, return a match that
    is equal to or less than max_dis edits away.

--- itsm/adsm.py
+++ itsm/adsm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from nltk import metrics, stem, tokenize
 from suds.sax.element import Attribute, Element
 
 from itsm.base import Base
@@ -62,7 +63,7 @@ class ADSMBase(Base):
 			principals = [principals]
 		return ADSMBase.ref_sep.join(filter(lambda x: x, map(self.person_ref, principals)))
 
-	def listitem_ref(self, list_uuid, view_uuid, field, field_value, display_field='_ows_Title', fuzzy=False):
+	def listitem_ref(self, list_uuid, view_uuid, field, field_value, display_field='_ows_Title', fuzzy=False, max_dist=4):
 		cache_key = '%s/%s/%s' % (list_uuid, view_uuid, field)
 		table = self.listitem_ref._cache.get(cache_key)
 		if not table:
@@ -72,18 +73,41 @@ class ADSMBase(Base):
 			          else []
 			table = dict(filter(lambda x: x[0], map(lambda x: (x.__dict__.get(field), x), list_items_rows)))
 			self.listitem_ref._cache[cache_key] = table
+
+		# Attempt to get exact match
 		match = table.get(field_value)
 		if match:
 			return '%s%s%s' % (match['_ows_ID'], ADSMBase.ref_sep, match[display_field] if display_field else '')
+
+		# If fuzzy is True, attempt to find a reasonable match
+		if fuzzy:
+			stemmer = stem.PorterStemmer()
+			def normalize(s):
+				words = tokenize.wordpunct_tokenize(s.lower().strip())
+				return ' '.join([stemmer.stem(w) for w in words])
+
+			normalized_cache_key = '%s/%s/%s/normalized' % (list_uuid, view_uuid, field)
+			normalized_table = self.listitem_ref._cache.get(normalized_cache_key)
+			if not normalized_table:
+				normalized_table = dict((normalize(k), v) for k, v in table.items())
+				self.listitem_ref._cache[normalized_cache_key] = normalized_table
+
+			normalized_field_value = normalize(field_value)
+			candidates = sorted(normalized_table.items(), lambda x, y: metrics.edit_distance(x, normalized_field_value) - metrics.edit_distance(y, normalized_field_value), lambda t: t[0])
+			if metrics.edit_distance(candidates[0][0], normalized_field_value) <= max_dist:
+				match = candidates[0][1]
+				return '%s%s%s' % (match['_ows_ID'], ADSMBase.ref_sep, match[display_field] if display_field else '')
+
+		# No match found
 		return None
 	listitem_ref._cache = {}
 
-	def listitem_refs(self, list_uuid, view_uuid, field, field_values, display_field='_ows_Title', fuzzy=False):
+	def listitem_refs(self, list_uuid, view_uuid, field, field_values, display_field='_ows_Title', fuzzy=False, max_dist=4):
 		if not field_values:
 			return None
 		if isinstance(field_values, basestring):
 			field_values = [field_values]
-		return ADSMBase.ref_sep.join(filter(lambda x: x, map(lambda y: self.listitem_ref(list_uuid, view_uuid, field, y, display_field=display_field, fuzzy=fuzzy), field_values)))
+		return ADSMBase.ref_sep.join(filter(lambda x: x, map(lambda y: self.listitem_ref(list_uuid, view_uuid, field, y, display_field=display_field, fuzzy=fuzzy, max_dist=max_dist), field_values)))
 
 	def reset_caches(self):
 		self.listitem_ref._cache = {}
