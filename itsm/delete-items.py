94e07d993a7a189b8b3056f9be0ca5434ee0ef12
King Chung Huang <kinghuang@mac.com>
2015-04-25 09:37:40 -0600

    Add script to delete items from ADSM lists

---
+++ itsm/delete-items.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from suds.sax.element import Attribute, Element
+
+from itsm.adsm import ADSMBase
+
+
+class DeleteItems(ADSMBase):
+
+	def argument_parser(self):
+		parser = super(DeleteItems, self).argument_parser()
+
+		parser.add_argument('list', help='list uuid')
+		parser.add_argument('-d', help='dry run', action='store_true')
+
+		return parser
+
+	def main(self):
+		items = self.adsm_lists.service.GetListItems(self.args.list).listitems.data.row
+
+		method_idx = 1
+		batch = Element('Batch')\
+		       .append(Attribute('OnError', 'Continue'))\
+		       .append(Attribute('ListVersion', 1))
+
+		def update(b):
+			if not self.args.d:
+				updates = Element('ns1:updates').append(b)
+				print self.adsm_lists.service.UpdateListItems(listName=self.args.list, updates=updates)
+
+		for item in items:
+			method = Element('Method')\
+			        .append(Attribute('ID', method_idx))\
+			        .append(Attribute('Cmd', 'Delete'))\
+			        .append(Element('Field')\
+			          .append(Attribute('Name', 'ID'))\
+			          .setText(item['_ows_ID']))
+			batch.append(method)
+			print method
+			method_idx += 1
+
+			if len(batch) > 20:
+				update(batch)
+				batch.detachChildren()
+
+		if len(batch) > 0:
+			update(batch)
+
+
+def main(args=None):
+	return DeleteItems(args=args).run()
+
+
+if __name__ == '__main__':
+	main()
